a89fee080ef246f118bdc36fce19d362c195901f
Miguel <miguelespigaresr@gmail.com>
2020-03-19 11:29:50 +0100

    dfd

--- popup.js
+++ popup.js
@@ -2,9 +2,9 @@
 Comprobar si lo que introducimos es un número 
  */
 
-function isNumber(evt){
+function isNumber(evt) {
     var charCode = (evt.which) ? evt.which : evt.keyCode
-    if ((charCode < 48 || charCode > 57 || charCode ==101))
+    if ((charCode < 48 || charCode > 57 || charCode == 101))
         return false;
     return true;
 }
@@ -13,18 +13,18 @@ Comprobar que sólo sean 3 caracteres
 */
 function maxLengthCheck(object) {
     if (object.value.length > object.maxLength)
-      object.value = object.value.slice(0, object.maxLength)
-  }
+        object.value = object.value.slice(0, object.maxLength)
+}
 
-document.addEventListener('DOMContentLoaded', function()
-{
-    document.getElementById("btnScroller").addEventListener('click', onclick, false);
-    
-    function onclick()
-    {
-        chrome.tabs.query({currentWindow: true, active: true},
-            function(tabs)
-            {
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.getElementById("btnScroller");
+
+    if (btn) {
+        btn.addEventListener('click', onclick, false);
+    }
+    function onclick() {
+        chrome.tabs.query({ currentWindow: true, active: true },
+            function (tabs) {
                 var seconds = document.getElementById("segundos").value;
                 chrome.tabs.sendMessage(tabs[0].id, seconds);
             });
