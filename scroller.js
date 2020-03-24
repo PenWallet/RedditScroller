62db611446b77af6a3e75232a8332998b33f7333
Unknown <carterasindinero@gmail.com>
2020-03-24 19:40:46 +0100

    Scroll a partir del primer post que tengas en pantalla

--- scroller.js
+++ scroller.js
@@ -8,9 +8,12 @@ chrome.runtime.onMessage.addListener(
     {
         if(seconds > 0)
         {
-            //We're going to start the iteration of divs
-            i = 0;
             divs = document.getElementsByClassName('rpBJOHq2PR60pnwJlUyP0')[0].childNodes;
+
+            //We're going to start the iteration of divs
+            setIndex();
+            
+            //Needed to scroll up the header height, so it doesn't overlap the post
             headerHeight = document.getElementsByClassName('_1tvdSTbdxaK-BnUbzUIqIY _2GyPfdsi-MbQFyHRECo9GO cx1ohrUAq6ARaXTX2u8YN   ')[0].offsetHeight * -1;
 
             //Start interval
@@ -46,4 +49,30 @@ function scrollPost()
         if(divs[i].offsetHeight != 0)
             break;
     }
+}
+
+//Thanks to @Tokimon in StackOverflow
+function checkVisible(elm) {
+    var rect = elm.getBoundingClientRect();
+    var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+    return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+function setIndex()
+{
+    var found = false;
+
+    for(j = 0; j < divs.length; j++)
+    {
+        if(checkVisible(divs[j]))
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if(found)
+        i = j;
+    else
+        i = 0;
 }
